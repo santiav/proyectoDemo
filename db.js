da1800a4bc3962fcf3a2013eee816b41eda206e5
Santi Acosta Verrier <santiav@gmail.com>
2022-11-20 18:54:50 -0300

    minor changes db

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
-let mysql      = require('mysql');
+let mysql      = require('mysql2');
 
 let connection = mysql.createConnection({
-	host     : process.env.DB_HOST,
-	user     : process.env.DB_USER,
-	password : process.env.DB_PASSWORD,
-	database : process.env.DB_DATABASE,
+	host     : process.env.DB_HOST || 'localhost',
+	user     : process.env.DB_USER || 'root',
+	password : process.env.DB_PASSWORD || '',
+	database : process.env.DB_DATABASE || "proyectoDemo",
 });
 
 connection.connect(function(err) {
